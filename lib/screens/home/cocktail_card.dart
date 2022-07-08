f44533cfd5af99b04271c555ef11af47cf1d6b1f
nicualex <nicualex@gmail.com>
2022-07-08 13:33:10 -0400

    updates3

--- lib/screens/home/cocktail_card.dart
+++ lib/screens/home/cocktail_card.dart
@@ -2,8 +2,6 @@
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:transparent_image/transparent_image.dart';
-import 'package:firebase_image/firebase_image.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:manhattan/constants.dart';
 
